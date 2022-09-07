06de5ebeb2eae639b2f5b15c46131dad0a44f4bc
Brad Heffernan <ph3onix83@gmail.com>
2022-09-08 02:40:54 +0930

    updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,9 +3,9 @@ import './header.css'
 
 const Header = (props) => {
   return (
-        <div className='header bg-dark'>
+        <div className='header bg-dark p-2'>
            <div className='container'>
-            <h1>Title Name of <span>page</span></h1>
+            <h1 className='m-0'>Title Name of <span>page</span></h1>
            </div>
         </div>
   );
